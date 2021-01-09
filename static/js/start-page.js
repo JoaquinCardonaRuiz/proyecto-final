2741fe41e361077cd845877d576389b1de7af91c
Bruno Tomás Caracini <bruno98980@gmail.com>
2021-01-09 03:21:23 -0300

    Responsive response incorporada a la start page.

--- static/js/start-page.js
+++ static/js/start-page.js
@@ -1,3 +1,11 @@
+
+if(parseInt($(window).width()) > 1550){
+    $("#page-content-1").css({"margin-top":"40vw"});
+}
+else{
+    $("#page-content-1").css({"margin-top":"32vw"});
+}
+
 function animations(){
     if(window.scrollY >= 430){
         $("#start-card1").fadeIn(800);
@@ -13,13 +21,12 @@ function animations(){
     }
 
     if(window.scrollY >= 1100){
-        $("#start-image2").fadeIn(1000);
-        $("#start-image2").css({"transform":"translateX(-20px)"});
+        $("#start-image2").fadeIn(1200);
+        $("#start-image2").css({"transform":"translateX(-40px)"});
     }
     else{
-        $("#start-image2").css({"transform":"translateX(20px)"});
+        $("#start-image2").css({"transform":"translateX(40px)"});
         $("#start-image2").fadeOut(500);
     }
-
     
 }
