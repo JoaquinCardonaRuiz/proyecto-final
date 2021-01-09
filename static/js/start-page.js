93314c798988ef5de2c1dba60ab394e032a7c8d6
Bruno Tomás Caracini <bruno98980@gmail.com>
2021-01-09 17:03:59 -0300

    Avances en landing page.

--- static/js/start-page.js
+++ static/js/start-page.js
@@ -11,8 +11,6 @@ function animations(){
         $("#start-card1").fadeIn(800);
         $("#start-card2").fadeIn(1300);
         $("#start-card3").fadeIn(1800);
-
-
     }
     else{
         $("#start-card1").hide();
@@ -21,11 +19,20 @@ function animations(){
     }
 
     if(window.scrollY >= 1100){
-        $("#start-image2").fadeIn(1200);
-        $("#start-image2").css({"transform":"translateX(-40px)"});
+        $("#start-image-flujo-basico").fadeIn(1000);
+        $("#start-image-flujo-basico").css({"transform":"translateX(50px)"});
     }
     else{
-        $("#start-image2").css({"transform":"translateX(40px)"});
+        $("#start-image-flujo-basico").css({"transform":"translateX(-50px)"});
+        $("#start-image-flujo-basico").fadeOut(500);
+    }
+
+    if(window.scrollY >= 1100){
+        $("#start-image2").fadeIn(1000);
+        $("#start-image2").css({"transform":"translateX(-50px)"});
+    }
+    else{
+        $("#start-image2").css({"transform":"translateX(50px)"});
         $("#start-image2").fadeOut(500);
     }
     
