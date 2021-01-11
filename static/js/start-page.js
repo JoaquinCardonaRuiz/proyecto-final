bafafb105feb3d41668acfff042ee0e2c1d6befe
Bruno Tomás Caracini <bruno98980@gmail.com>
2021-01-11 18:27:02 -0300

    Green board landing page terminado.

--- static/js/start-page.js
+++ static/js/start-page.js
@@ -9,6 +9,8 @@ else{
 
 function animations(){
     //alert(window.scrollY);
+
+    //Animación sección tarjetas
     if(window.scrollY >= 430){
         $("#start-card1").fadeIn(800);
         $("#start-card2").fadeIn(1300);
@@ -20,6 +22,7 @@ function animations(){
         $("#start-card3").hide();
     }
 
+    //Animación sección flujo básico
     if(window.scrollY >= 1100){
         $("#start-image-flujo-basico").fadeIn(1000);
         $("#start-image-flujo-basico").css({"transform":"translateX(75px)"});
@@ -28,8 +31,9 @@ function animations(){
         $("#start-image-flujo-basico").css({"transform":"translateX(-75px)"});
         $("#start-image-flujo-basico").fadeOut(500);
     }
-
-    if(window.scrollY >= 1100){
+    
+    //Animación sección plantas
+    if(window.scrollY >= 1555){
         $("#start-image-plantas").fadeIn(1000);
         $("#start-image-plantas").css({"transform":"translateX(-50px)"});
     }
@@ -38,7 +42,9 @@ function animations(){
         $("#start-image-plantas").fadeOut(500);
     }
 
-    if(window.scrollY >= 1500){
+    //Animación sección green board
+    pi_greenBoard = 1500
+    if(window.scrollY >= pi_greenBoard){
         $("#page-content-title-green-board").fadeIn(500);
         $("#page-content-title-green-board").css({"transform":"translateY(-90px)"});
     }
@@ -48,21 +54,23 @@ function animations(){
     
     }
 
-    if(window.scrollY >= 1750){
+    if(window.scrollY >= pi_greenBoard + 250){
         $("#row-page-content-green-board-1").fadeIn(500);
         $("#row-page-content-green-board-1").css({"transform":"translateX(30px)"});
+        if(window.scrollY >= pi_greenBoard + 450){
+            $("#row-page-content-green-board-2").fadeIn(500);
+            $("#row-page-content-green-board-2").css({"transform":"translateX(-30px)"});
+            if(window.scrollY >= pi_greenBoard + 650){
+                $("#row-page-content-green-board-3").fadeIn(500);
+                $("#row-page-content-green-board-3").css({"transform":"translateX(30px)"});
+            }
+        }
     }
     else{
         $("#row-page-content-green-board-1").css({"transform":"translateX(-65px)"});
         $("#row-page-content-green-board-1").fadeOut(500);
-    
-    }
-
-    if(window.scrollY >= 1750){
-        $("#row-page-content-green-board-3").fadeIn(500);
-        $("#row-page-content-green-board-3").css({"transform":"translateX(30px)"});
-    }
-    else{
+        $("#row-page-content-green-board-2").css({"transform":"translateX(65px)"});
+        $("#row-page-content-green-board-2").fadeOut(500);
         $("#row-page-content-green-board-3").css({"transform":"translateX(-65px)"});
         $("#row-page-content-green-board-3").fadeOut(500);
     
