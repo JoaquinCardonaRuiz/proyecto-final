f5229662d8c04fe8c674401777f94fd7268384d8
JoaquinCardonaRuiz <joaquincardonaruiz@gmail.com>
2021-01-16 14:13:56 -0300

    Finalizado interfaz de pestanas

--- static/js/gestion-ed.js
+++ static/js/gestion-ed.js
@@ -1,3 +1,6 @@
+var del = false;
+var mod = false;
+
 function openLoadingRing(){
     document.getElementById("open-loading-modal").click();
     $(".lds-ring").hide();
@@ -129,24 +132,160 @@ function getTablaSalidas(id, nombre){
 }
 
 
-function validaNombre(nombres){
+function validaNuevoNombre(nombres){
     var n = document.getElementById("nombreInput").value;
     if(nombres.includes(n)){
         //Se comprueba regla RN11
-        document.getElementById("descuentoNivelError").innerHTML = "* Ese nombre ya ha sido registrado como una entidad de destino.";
+        document.getElementById("nombreEntidadError").innerHTML = "* Ese nombre ya ha sido registrado como una entidad de destino.";
         document.getElementsByName("add-btn")[0].disabled = true;
     }
     else if (!n){
         //Se comprueba regla RN12
-        document.getElementById("descuentoNivelError").innerHTML = "* Este campo debe ser completado para crear la entidad.";
+        document.getElementById("nombreEntidadError").innerHTML = "* Este campo debe ser completado para crear la entidad.";
         document.getElementsByName("add-btn")[0].disabled = true;
     }
     else{
-        document.getElementById("descuentoNivelError").innerHTML = "";
+        document.getElementById("nombreEntidadError").innerHTML = "";
         document.getElementsByName("add-btn")[0].disabled = false;
     }
 }
 
 function submitForm(n){
     document.getElementById(n).submit();
+}
+
+function modifyEntidad(){
+    if (mod == false){
+        $('.delete-row').hide()
+        $('.delete-th').hide()
+        $('.modify-row').fadeIn()
+        $('.modify-th').fadeIn()
+        mod = true;
+        del = false;
+        $('#option-right').css('border', '2px solid transparent');
+        $('#option-middle').css('border', '2px solid #95C22B');
+        var y = window.scrollY + document.querySelector('#table-container').getBoundingClientRect().top; // Y
+        var x = window.scrollX + document.querySelector('#table-container').getBoundingClientRect().left; // X
+        window.scrollTo(x, y);
+        
+    }
+    else{
+        $('.modify-row').fadeOut()
+        $('.modify-th').fadeOut()
+        mod = false;
+        $('#option-middle').css('border', '2px solid transparent');
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+}
+
+function removeEntidad(){
+    if (del == false){
+        $('.modify-row').hide()
+        $('.modify-th').hide()
+        $('.delete-row').fadeIn()
+        $('.delete-th').fadeIn()
+        del = true;
+        mod = false;
+        $('#option-middle').css('border', '2px solid transparent');
+        $('#option-right').css('border', '2px solid #95C22B');
+        var y = window.scrollY + document.querySelector('#table-container').getBoundingClientRect().top; // Y
+        var x = window.scrollX + document.querySelector('#table-container').getBoundingClientRect().left; // X
+        window.scrollTo(x, y);
+        
+    }
+    else{
+        $('.delete-row').fadeOut()
+        $('.delete-th').fadeOut()
+        del = false;
+        $('#option-right').css('border', '2px solid transparent');
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+}
+
+//Abre el Modal de baja. 
+function openBajaModal(idEntidad){
+    //Manejo de elementos de carga
+    $("#fieldsRowBaja").show();
+    $(".lds-ring").hide();
+    $('#bottomBajaModalText').hide();
+    $('#primary-btn-alert').prop('disabled', false);
+    $('#secondary-btn-baja').prop('disabled', false);
+
+    //Manejo de carteles
+    jQuery.noConflict();
+    $('#primary-btn-alert').prop('disabled', false); 
+    $('#idEntidad').val(String(idEntidad));
+    $('#bajaEntidadModal').modal('show');
+}
+
+function baja_entidad(){
+
+    //Manejo de elementos para la carga
+    $("#fieldsRowBaja").hide();
+    $(".lds-ring div").css("border-color", "#cf4545 transparent transparent transparent");
+    $(".lds-ring").show().fadeIn(500);
+    $('#bottomBajaModalText').show();
+    $('#primary-btn-alert').prop('disabled', true);
+    $('#secondary-btn-baja').prop('disabled', true);
+
+    //Manejo de datos
+    id = $('#idEntidad').val();
+    window.location.href='/gestion-ed/baja' + String(id)
+
+    //Funcion que va cambiando los mensajes de carga.
+    nextMsgBaja()
+}
+
+function nextMsgBaja() {
+    if (messages.length == 1) {
+        $('#bottomBajaModalText').html(messagesBaja.pop()).fadeIn(500);
+
+    } else {
+        $('#bottomBajaModalText').html(messagesBaja.pop()).fadeIn(500).delay(10000).fadeOut(500, nextMsgBaja);
+    }
+};
+
+
+var messagesBaja = [
+    "Estamos eliminando la entidad de destino...",
+    "¡Casi listo! Últimos retoques"
+].reverse();
+
+
+function openEditModal(idEntidad,nombreEntidad){
+    jQuery.noConflict();
+    $(".lds-ring").hide();
+    $('#idEntidad').val(String(idEntidad))
+    $('#nombreEntidad').val(String(nombreEntidad));
+    document.getElementById("EntNombreInput").value = nombreEntidad;
+    $('#editEntidadModal').modal('show');
+    $('.nav-tabs a:first').tab('show');
+    document.getElementById("mod-name-btn").disabled = true;
+}
+
+function validaModNombre(nombres){
+    var n = document.getElementById("EntNombreInput").value;
+    var nombreA = document.getElementById("nombreEntidad").value;
+    console.log(n);
+    console.log(nombreA);
+    if(n == nombreA){
+        document.getElementById("modNombreEntidadError").innerHTML = "* El nombre de la entidad debe ser distinto a su nombre anterior.";
+        document.getElementById("mod-name-btn").disabled = true;
+    }
+    else if(nombres.includes(n)){
+        //Se comprueba regla RN11
+        document.getElementById("modNombreEntidadError").innerHTML = "* Ese nombre ya ha sido registrado como una entidad de destino.";
+        document.getElementById("mod-name-btn").disabled = true;
+    }
+    else if (!n){
+        //Se comprueba regla RN12
+        document.getElementById("modNombreEntidadError").innerHTML = "* Este campo debe ser completado para modificar la entidad.";
+        document.getElementById("mod-name-btn").disabled = true;
+    }
+    else{
+        document.getElementById("modNombreEntidadError").innerHTML = "";
+        document.getElementById("mod-name-btn").disabled = false;
+    }
 }
