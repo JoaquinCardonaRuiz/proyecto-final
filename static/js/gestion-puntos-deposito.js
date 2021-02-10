2bcccdb47c29eff9c2f3eb6d6e512b34cfd3b6e3
Bruno Tomás Caracini <bruno98980@gmail.com>
2021-02-10 03:16:17 -0300

    Avances modificar modal.

--- static/js/gestion-puntos-deposito.js
+++ static/js/gestion-puntos-deposito.js
@@ -558,6 +558,14 @@ function labelPosition(){
 
     $("#pdInactivo").css({top: pos_switch - 24, position:'absolute'});
     $("#pdInactivo").css({left: pos_switch_left + 140, position:'absolute'});
+
+    var pos_switch = document.getElementById("customSwitch2").offsetTop;
+    var pos_switch_left = document.getElementById("customSwitch2").offsetLeft;
+    $("#pdActivoMod").css({top: pos_switch - 24, position:'absolute'});
+    $("#pdActivoMod").css({left: pos_switch_left + 140, position:'absolute'});
+
+    $("#pdInactivoMod").css({top: pos_switch - 24, position:'absolute'});
+    $("#pdInactivoMod").css({left: pos_switch_left + 140, position:'absolute'});
 }
 
 //Habilita y deshabilita los horarios para un día determinado.
@@ -778,178 +786,71 @@ function updateMap(){
 
 function openModModal(){
     jQuery.noConflict();
+    
+    //Define que se debe mostrar y que se oculta.
+
+
+    //Seteo de valores iniciales.
+
 
     $("#modPDModal").modal("show");
 }
+
+function configureModalTab(mod_form_page){
+    if (mod_form_page == 1){
+        $("#modal-mod-p3").hide();
+        $("#modal-mod-p2").hide();
+        $("#modal-mod-p4").hide();
+        $("#modal-mod-p1").fadeIn();
+        $("#subheader-mod").text("Datos Básicos");
+        goToTopOfPage();
+     }
+     else if (mod_form_page == 2){
+        $("#modal-mod-p1").hide();
+        $("#modal-mod-p3").hide();
+        $("#modal-mod-p4").hide();
+        $("#modal-mod-p2").fadeIn();
+        $("#subheader-mod").text("Dirección");
+        goToTopOfPage();
+     }
+     else if (mod_form_page == 3){
+        $("#modal-alta-p1").hide();
+        $("#modal-alta-p2").hide();
+        $("#modal-alta-p4").hide();
+        $("#modal-alta-p3").fadeIn();
+        $("#secondary-btn").text("Anterior");
+        $("#primary-btn-alta").text("Siguiente");
+        $("#subheader-alta").text("Horarios");
+        $("#bottomAltaModalTextAltaPD").css({"transform":"translateY(0px)"});
+        $(".margin-row").hide();
+        $("#bottomAltaModalTextAltaPD").text('Una vez elegidos los horarios, presione el botón "Siguiente".');
+        goToTopOfPage();
+     } 
+     else{
+        closeMenu();
+        labelShowHide();
+        $("#modal-alta-p1").hide();
+        $("#modal-alta-p2").hide();
+        $("#modal-alta-p3").hide();
+        $("#modal-alta-p4").fadeIn();
+        $("#secondary-btn").text("Anterior");
+        $("#primary-btn-alta").text("Crear Punto de  Depósito");
+        $("#subheader-alta").text("Materiales");
+        $("#bottomAltaModalTextAltaPD").text('Una vez elegidos los materiales, presione "Crear Punto de  Depósito" para añadir el nuevo Punto.');
+        goToTopOfPage();
+     }
+}
+$("#customSwitch2").click(function() {
+    if($("#customSwitch2").is(":checked") == true){
+        $("#pdInactivoMod").fadeOut();    
+        $("#pdActivoMod").fadeIn();
+        $("#switch-value-mod").val("true");
+    }
+    else{
+        $("#pdActivoMod").fadeOut(); 
+        $("#pdInactivoMod").fadeIn();
+        $("#switch-value-mod").val("false");
+    }
+});
+
 labelPosition();
-
-
-
-
-//DOM elements
-const DOMstrings = {
-    stepsBtnClass: 'multisteps-form__progress-btn',
-    stepsBtns: document.querySelectorAll(`.multisteps-form__progress-btn`),
-    stepsBar: document.querySelector('.multisteps-form__progress'),
-    stepsForm: document.querySelector('.multisteps-form__form'),
-    stepsFormTextareas: document.querySelectorAll('.multisteps-form__textarea'),
-    stepFormPanelClass: 'multisteps-form__panel',
-    stepFormPanels: document.querySelectorAll('.multisteps-form__panel'),
-    stepPrevBtnClass: 'js-btn-prev',
-    stepNextBtnClass: 'js-btn-next' };
-  
-  
-  //remove class from a set of items
-  const removeClasses = (elemSet, className) => {
-  
-    elemSet.forEach(elem => {
-  
-      elem.classList.remove(className);
-  
-    });
-  
-  };
-  
-  //return exect parent node of the element
-  const findParent = (elem, parentClass) => {
-  
-    let currentNode = elem;
-  
-    while (!currentNode.classList.contains(parentClass)) {
-      currentNode = currentNode.parentNode;
-    }
-  
-    return currentNode;
-  
-  };
-  
-  //get active button step number
-  const getActiveStep = elem => {
-    return Array.from(DOMstrings.stepsBtns).indexOf(elem);
-  };
-  
-  //set all steps before clicked (and clicked too) to active
-  const setActiveStep = activeStepNum => {
-  
-    //remove active state from all the state
-    removeClasses(DOMstrings.stepsBtns, 'js-active');
-  
-    //set picked items to active
-    DOMstrings.stepsBtns.forEach((elem, index) => {
-  
-      if (index <= activeStepNum) {
-        elem.classList.add('js-active');
-      }
-  
-    });
-  };
-  
-  //get active panel
-  const getActivePanel = () => {
-  
-    let activePanel;
-  
-    DOMstrings.stepFormPanels.forEach(elem => {
-  
-      if (elem.classList.contains('js-active')) {
-  
-        activePanel = elem;
-  
-      }
-  
-    });
-  
-    return activePanel;
-  
-  };
-  
-  //open active panel (and close unactive panels)
-  const setActivePanel = activePanelNum => {
-  
-    //remove active class from all the panels
-    removeClasses(DOMstrings.stepFormPanels, 'js-active');
-  
-    //show active panel
-    DOMstrings.stepFormPanels.forEach((elem, index) => {
-      if (index === activePanelNum) {
-  
-        elem.classList.add('js-active');
-  
-        setFormHeight(elem);
-  
-      }
-    });
-  
-  };
-  
-  //set form height equal to current panel height
-  const formHeight = activePanel => {
-  
-    const activePanelHeight = activePanel.offsetHeight;
-  
-    DOMstrings.stepsForm.style.height = `${activePanelHeight}px`;
-  
-  };
-  
-  const setFormHeight = () => {
-    const activePanel = getActivePanel();
-  
-    formHeight(activePanel);
-  };
-  
-  //STEPS BAR CLICK FUNCTION
-  DOMstrings.stepsBar.addEventListener('click', e => {
-  
-    //check if click target is a step button
-    const eventTarget = e.target;
-  
-    if (!eventTarget.classList.contains(`${DOMstrings.stepsBtnClass}`)) {
-      return;
-    }
-  
-    //get active button step number
-    const activeStep = getActiveStep(eventTarget);
-  
-    //set all steps before clicked (and clicked too) to active
-    setActiveStep(activeStep);
-  
-    //open active panel
-    setActivePanel(activeStep);
-  });
-  
-  //PREV/NEXT BTNS CLICK
-  DOMstrings.stepsForm.addEventListener('click', e => {
-  
-    const eventTarget = e.target;
-  
-    //check if we clicked on `PREV` or NEXT` buttons
-    if (!(eventTarget.classList.contains(`${DOMstrings.stepPrevBtnClass}`) || eventTarget.classList.contains(`${DOMstrings.stepNextBtnClass}`)))
-    {
-      return;
-    }
-  
-    //find active panel
-    const activePanel = findParent(eventTarget, `${DOMstrings.stepFormPanelClass}`);
-  
-    let activePanelNum = Array.from(DOMstrings.stepFormPanels).indexOf(activePanel);
-  
-    //set active step and active panel onclick
-    if (eventTarget.classList.contains(`${DOMstrings.stepPrevBtnClass}`)) {
-      activePanelNum--;
-  
-    } else {
-  
-      activePanelNum++;
-  
-    }
-  
-    setActiveStep(activePanelNum);
-    setActivePanel(activePanelNum);
-  
-  });
-  
-  //SETTING PROPER FORM HEIGHT ONLOAD
-  window.addEventListener('load', setFormHeight, false);
-  
-  //SETTING PROPER FORM HEIGHT ONRESIZE
-  window.addEventListener('resize', setFormHeight, false);
