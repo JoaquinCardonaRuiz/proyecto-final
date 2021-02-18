dbcbc3854501306a0bc350046a84c1d8f8541cb8
Bruno Tomás Caracini <bruno98980@gmail.com>
2021-02-18 03:56:33 -0300

    Avances front-end del login.

--- static/js/login.js
+++ static/js/login.js
@@ -4,33 +4,25 @@ $( window ).resize(function() {
     reduceWindow();
 });
 
+$(".content-container").fadeIn();
+$(".content-container").css({"transform":"translateX(7rem)"});
+
+
 function reduceWindow(){
+    if($( window ).width() < 1050 && $( window ).width() > 800){
+        $(".content-container").css({"transform":"translateX(4rem)"});
+    }
     if ($( window ).width() < 800){
         if (img_hide == false){
             $(".img-login").fadeOut();
-            $(".login-container").css({"transform":"translateX(-250px)"});
-            $("#register-label").css({"transform":"translateX(70px)"});
-            $("#register-label").css({"width":"135%"});
-
-            $(".ingresar-label").css({"transform":"translateX(70px)"});
-
-            $(".buttons-container").css({"width":"130%"});
-            $(".buttons-container").css({"transform":"translateX(-47.5px)"});
-            $(".img-container").css({"transform":"translateX(37.5px)"});
+            $(".content-container").css({"transform":"translateX(-57%)"});
             img_hide = true;
-            olvido-password
         }
     }
     else if ($( window ).width() > 800){
         if (img_hide == true){
-            $("#register-label").css({"transform":"translateX(0px)"});
-            $("#register-label").css({"width":"96%"});
-            $(".img-login").fadeIn();
-            $(".login-container").css({"transform":"translateX(0px)"});
-            $(".img-container").css({"transform":"translateX(0px)"});
-            $(".buttons-container").css({"width":"38%"});
-            $(".buttons-container").css({"transform":"translateX(0px)"});
-            $(".ingresar-label").css({"transform":"translateX(0px)"});
+            $(".img-login").fadeIn(1000);
+            $(".content-container").css({"transform":"translateX(7rem)"});
 
 
             img_hide = false;
