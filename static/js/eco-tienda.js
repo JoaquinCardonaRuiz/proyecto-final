3c06dba30d2f262260f807693e266d1cb31dcd2b
Bruno Tomás Caracini <bruno98980@gmail.com>
2021-02-21 18:25:35 -0300

    Ahora es responsive.

---
+++ static/js/eco-tienda.js
@@ -0,0 +1,18 @@
+$( window ).resize(function() {
+    cambia_ancho_tarjetas();
+    //alert($( window ).width());
+});
+
+function cambia_ancho_tarjetas(){
+    if ($( window ).width() > 1440){
+        $(".card-column").css({"max-width":"25%"});
+    }
+    else if (($( window ).width() > 578)){
+        $(".card-column").css({"max-width":"33.33%"});
+    }
+    else{
+        $(".card-column").css({"max-width":"100%"});
+    }
+}
+
+cambia_ancho_tarjetas();
