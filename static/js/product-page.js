d48b55b44a4e3aa106e292ed70a6394c1020beb8
Bruno Tomás Caracini <bruno98980@gmail.com>
2021-02-22 19:43:41 -0300

    slide terminado.

--- static/js/product-page.js
+++ static/js/product-page.js
@@ -4,3 +4,11 @@ $(document).ready(function () {
       $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
     });
   });
+
+
+function setPvalues(value){
+    $("#cantEP").text(String(value) + " EcoPuntos");
+    $("#cantMoney").text("$" + String(100-value) + " ARS");
+}
+
+setPvalues(0);
