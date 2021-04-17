b7690c5284dab65b5dba32337a086c6278f347fb
Bruno Tomás Caracini <bruno98980@gmail.com>
2021-04-17 00:01:53 -0300

    bug corregido.

--- data/data_reportes.py
+++ data/data_reportes.py
@@ -32,7 +32,7 @@ class DatosReportes(Datos):
                 fecha_min = res[0]
             num_months = (d.year - fecha_min.year) * 12 + (d.month - fecha_min.month)
             print(fecha_min.month - d.month + 1)
-            for i in range(0,int(cant_meses)+1):
+            for i in range(0,int(cant_meses)):
                 #valido si es el mes actual, en cuyo caso, aplica el stock actual.
                 if current_month == start_month and current_year == start_year:
                     data.append(cant)
